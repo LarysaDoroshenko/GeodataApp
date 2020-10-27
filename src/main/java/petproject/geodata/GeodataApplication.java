20e137a1ae29aaf0bb259906ad46232571504a29
Larysa Doroshenko <larysad785@gmail.com>
2020-10-27 13:36:01 +0200

    refactor: introduce separate configuration classes

--- src/main/java/petproject/geodata/GeodataApplication.java
+++ src/main/java/petproject/geodata/GeodataApplication.java
@@ -2,21 +2,14 @@ package petproject.geodata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
 public class GeodataApplication {
-	
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(GeodataApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GeodataApplication.class, args);
+    }
 
 }
