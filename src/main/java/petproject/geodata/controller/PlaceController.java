6d6d36cd259b519c9beba3e28398798398a30e20
Larysa Doroshenko <larysad785@gmail.com>
2020-10-28 10:20:29 +0200

    feat: add degree validation for latitude and longitude

--- src/main/java/petproject/geodata/controller/PlaceController.java
+++ src/main/java/petproject/geodata/controller/PlaceController.java
@@ -3,6 +3,7 @@ package petproject.geodata.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import petproject.geodata.dto.PlaceDto;
 import petproject.geodata.service.PlaceService;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 import java.util.Optional;
 
+@Validated
 @RestController
 @RequestMapping(value = "/place")
 @RequiredArgsConstructor
@@ -21,8 +25,9 @@ public class PlaceController {
     private final PlaceService placeService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Optional<PlaceDto> findPlaceAndSave(@RequestParam(name = "lat") Double latitude,
-                                               @RequestParam(name = "lon") Double longitude) throws JsonProcessingException {
+    public Optional<PlaceDto> findPlaceAndSave(@RequestParam(name = "lat") @Min(value = -180) @Max(value = 180) Double latitude,
+                                               @RequestParam(name = "lon") @Min(value = -90) @Max(value = 90) Double longitude)
+            throws JsonProcessingException {
 
         return placeService.findPlaceOrFindAndSaveIfNotYetSaved(latitude, longitude);
     }
